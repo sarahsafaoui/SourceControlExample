c4d03f23dcfc34eecb180c5076c173010cb3eafd
sarah <sarsafaoui@gmail.com>
2024-07-22 10:35:32 -0700

    Initial Commit

---
+++ SourceControlExample/SourceControlExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlExampleApp.swift
+//  SourceControlExample
+//
+//  Created by sarah safaoui on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
